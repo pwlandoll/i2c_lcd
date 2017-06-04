37be82a50bbf1a4c19b1abb296526a6c7ab0eb57
Peter Landoll <pwlandoll@gmail.com>
2017-06-04 14:16:06 -0400

    Small changes to init and main file

--- i2c_lcd/__init__.py
+++ i2c_lcd/__init__.py
@@ -4,4 +4,6 @@ __author__ = """Peter Landoll"""
 __email__ = 'pwlandoll@gmail.com'
 __version__ = '0.1.0'
 
-from i2c_lcd import lcd
+from .i2c_lcd import lcd
+
+__all__ = ['lcd']
--- i2c_lcd/i2c_lcd.py
+++ i2c_lcd/i2c_lcd.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """
-GPL-licensed code found on GitHub Gists, contained in its entirety below,
-with cosmetic modifications. Taken 2017-3-26.
+GPL-licensed code found on GitHub Gists, contained below with some slight
+adjustments. Taken 2017-3-26.
 
 https://gist.github.com/DenisFromHR/cc863375a6e19dce359d#file-rpi_i2c_driver-py
 """
@@ -23,41 +23,8 @@ Made available under GNU GENERAL PUBLIC LICENSE
 import smbus
 from time import *
 
-
-class i2c_device:
-    def __init__(self, addr, port=1):
-        self.addr = addr
-        self.bus = smbus.SMBus(port)
-
-    # Write a single command
-    def write_cmd(self, cmd):
-        self.bus.write_byte(self.addr, cmd)
-        sleep(0.0001)
-
-    # Write a command and argument
-    def write_cmd_arg(self, cmd, data):
-        self.bus.write_byte_data(self.addr, cmd, data)
-        sleep(0.0001)
-
-    # Write a block of data
-    def write_block_data(self, cmd, data):
-        self.bus.write_block_data(self.addr, cmd, data)
-        sleep(0.0001)
-
-    # Read a single byte
-    def read(self):
-        return self.bus.read_byte(self.addr)
-
-    # Read
-    def read_data(self, cmd):
-        return self.bus.read_byte_data(self.addr, cmd)
-
-    # Read a block of data
-    def read_block_data(self, cmd):
-        return self.bus.read_block_data(self.addr, cmd)
-
-
 # LCD Address
+# Default to 0x27, for RPi3 I2C device
 ADDRESS = 0x27
 
 # commands
@@ -105,6 +72,39 @@ LCD_NOBACKLIGHT = 0x00
 En = 0b00000100 # Enable bit
 Rw = 0b00000010 # Read/Write bit
 Rs = 0b00000001 # Register select bit
+
+
+class i2c_device:
+    def __init__(self, addr=ADDRESS, port=1):
+        self.addr = addr
+        self.bus = smbus.SMBus(port)
+
+    # Write a single command
+    def write_cmd(self, cmd):
+        self.bus.write_byte(self.addr, cmd)
+        sleep(0.0001)
+
+    # Write a command and argument
+    def write_cmd_arg(self, cmd, data):
+        self.bus.write_byte_data(self.addr, cmd, data)
+        sleep(0.0001)
+
+    # Write a block of data
+    def write_block_data(self, cmd, data):
+        self.bus.write_block_data(self.addr, cmd, data)
+        sleep(0.0001)
+
+    # Read a single byte
+    def read(self):
+        return self.bus.read_byte(self.addr)
+
+    # Read
+    def read_data(self, cmd):
+        return self.bus.read_byte_data(self.addr, cmd)
+
+    # Read a block of data
+    def read_block_data(self, cmd):
+        return self.bus.read_block_data(self.addr, cmd)
 
 
 class lcd:
